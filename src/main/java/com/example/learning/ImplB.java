e21d39568a30064907a2e76631297a412d004f83
Snowman007boom <406353390@qq.com>
2021-11-20 17:22:50 +0800

    first

---
+++ src/main/java/com/example/learning/ImplB.java
@@ -0,0 +1,10 @@
+package com.example.learning;
+
+public class ImplB implements B{
+    @Override
+    public String rule() {
+        System.out.println("我是B");
+        return "im" +
+                " ok  低调";
+    }
+}
